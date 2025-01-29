2ae66f78ed482d759c5524df2c9c8502949fc5c3
Alex5727 <alejandrogrjalva@gmail.com>
2025-01-29 14:41:09 -0700

    Agregar archivos de proyecto.

---
+++ Pantalla_Principal.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Proyecto_TROU
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
