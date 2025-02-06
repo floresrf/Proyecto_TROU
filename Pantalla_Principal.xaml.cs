d909484cfbf92cfa027ad281fcc120e1b5086311
floresrf <arfr10@hotmail.com>
2025-02-06 08:51:21 -0700

    #

--- Pantalla_Principal.xaml.cs
+++ Pantalla_Principal.xaml.cs
@@ -9,9 +9,22 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void PushButton_Pressed(object sender, EventArgs e)
         {
+            var color = PushButton.BackgroundColor.ToHex();
 
+            if (color == "#FF0000") //Red
+            {                
+                PushButton.BackgroundColor = Color.FromArgb("#0f0"); //Green
+                StatusLabel.Text = "Abrido";
+                StatusLabel.TextColor = Color.FromArgb("#0f0");
+            }
+            else
+            {
+                PushButton.BackgroundColor = Color.FromArgb("#f00"); //Red again
+                StatusLabel.Text = "Cerrado";
+                StatusLabel.TextColor = Color.FromArgb("#f00");
+            }
         }
     }
 
